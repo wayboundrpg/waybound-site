0fe80175e722fa178b6381bd276b1ed123ef08ed
wayboundrpg <wayboundrpg@gmail.com>
2025-04-28 15:38:06 +0100

    WIP: Phase 2 setup (Tailwind & Vite) - needs review

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./index.html",
+      "./tools/**/*.html",
+      "./styles/**/*.css"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
