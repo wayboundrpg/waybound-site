31767fec967db7fe207b5546638757a47e7fe0f2
wayboundrpg <wayboundrpg@gmail.com>
2025-04-28 15:51:29 +0100

    trying

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    plugins: {
-      "@tailwindcss/postcss": {},
-      autoprefixer: {},
-    },
+    plugins: [
+      require("@tailwindcss/postcss"),
+      require("autoprefixer"),
+    ]
   };
   
